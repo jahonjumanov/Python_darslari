9cc1b79ad82361d8c7edd2ed799d5120853a614a
jahonjumanov <jumanovjahongir@gmail.com>
2021-07-12 11:49:20 +0500

    funksiya

---
+++ 12.07.2021 rekursiyalar/rekursiya malumot.py
@@ -0,0 +1,22 @@
+"""# qo'shish funksiya
+a=1
+b=2
+def qoshish(a,b):
+    return a+b
+print(qoshish(a,b))"""
+"""# Ayirish funksiya
+a=5
+b=2
+def ayirish(a,b):
+    return a-b
+print(ayirish(a,b))"""
+"""a=int(input("a="))
+b=int(input("b="))
+def qoshish(a,b):
+    return a+b
+def urta_arifmetig(a,b):
+    return qoshish(a,b)/2
+print(urta_arifmetig(a,b))"""
+
+
+
