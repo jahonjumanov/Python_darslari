2ce0fda3b4f491ce0d36be223c89a4d67d4a9b02
jahonjumanov <jumanovjahongir@gmail.com>
2021-07-13 10:37:00 +0500

    rekursiya

--- 12.07.2021 rekursiyalar/rekursiya malumot.py
+++ 12.07.2021 rekursiyalar/rekursiya malumot.py
@@ -17,6 +17,16 @@ def qoshish(a,b):
 def urta_arifmetig(a,b):
     return qoshish(a,b)/2
 print(urta_arifmetig(a,b))"""
+"""rekursiya ->o'ziga murojat qiladigan funksiya ekan"""
+# n dan 1 gacha bo'lgan natural sonlarni teskari tartibda ekranga chiqarish
+def birdanNgacha(n):
+    if n==1:
+        print(n)
+    else:
+        print(n)
+        birdanNgacha(n-2)
+birdanNgacha(5)
+
 
 
 
